37e91683fbabd7694959fba485eace8791830dbf
Alexander Rubinov <alexander.rubinov@web.de>
2015-11-08 01:00:20 +0100

    pr2

---
+++ src/main/java/org/thoughtworks/assessment/merchant/numberregistry/api/LocalNumberLiteralsRegistry.java
@@ -0,0 +1,15 @@
+package org.thoughtworks.assessment.merchant.numberregistry.api;
+
+import org.thoughtworks.assessment.merchant.numberregistry.LocalNumberLiteralsRegistryImpl.UnknownLiteral;
+import org.thoughtworks.assessment.merchant.numberregistry.api.types.LocalNumber;
+import org.thoughtworks.assessment.merchant.numberregistry.api.types.LocalNumberLiteral;
+import org.thoughtworks.assessment.merchant.romannumerals.api.types.RomanNumber;
+import org.thoughtworks.assessment.merchant.romannumerals.api.types.symbols.RomanNumberLiteral;
+
+public interface LocalNumberLiteralsRegistry {
+
+    public void registerLocalLiteral(LocalNumberLiteral localLiteral, RomanNumberLiteral romanLiteral);
+
+    public RomanNumber toRomanNumber(LocalNumber localNumber) throws UnknownLiteral;
+
+}
